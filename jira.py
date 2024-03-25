f2b7ee134e185b997f4b10230f9267ff802994ed
aniketmore620 <moreaniket620@gmail.com>
2024-03-25 13:59:09 +0530

    code-

--- jira.py
+++ jira.py
@@ -6,7 +6,7 @@ import json
 
 url = "https://moreaniket620.atlassian.net/rest/api/3/project"
 
-API_TOKEN="ATATT3xFfGF0MuX3YNTSZU8GGkp4do5sj6lXEIsOuCFzhJCBEsbJP0MgB9wYV-9lNnGg1FyowriYB7onSf6Qui94pH2IppAcPc547tyi2mCgs1iXIr_IGjDseMLpxLBTCd8w-sSNDAZ9CQdTAnmRbI_Xkph9WrLM8LnszrzUEevJZ_v2gxHQ99Y=ECE2BE2C"
+API_TOKEN="enter-jira-toekn"
 auth = HTTPBasicAuth("moreaniket620@gmail.com", API_TOKEN)
 
 headers = {
