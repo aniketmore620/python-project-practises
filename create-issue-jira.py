7f4ff6643e93f66f83287b458b612fa79787fcb5
aniketmore620 <moreaniket620@gmail.com>
2024-03-25 13:54:51 +0530

    updated code

--- create-issue-jira.py
+++ create-issue-jira.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 import json
 
 url = "https://moreaniket620.atlassian.net//rest/api/3/issue"
-API_TOKEN="ATATT3xFfGF0MuX3YNTSZU8GGkp4do5sj6lXEIsOuCFzhJCBEsbJP0MgB9wYV-9lNnGg1FyowriYB7onSf6Qui94pH2IppAcPc547tyi2mCgs1iXIr_IGjDseMLpxLBTCd8w-sSNDAZ9CQdTAnmRbI_Xkph9WrLM8LnszrzUEevJZ_v2gxHQ99Y=ECE2BE2C"
+API_TOKEN="<enter-api-token-of-jira"
 auth = HTTPBasicAuth("moreaniket620@gmail.com", API_TOKEN)
 
 headers = {
